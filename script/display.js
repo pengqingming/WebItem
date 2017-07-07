d55a72f6d6b0d233da4cb3fcdc62643b5bf7f8f2
pengqingming <peng15573439844@163.com>
2017-07-07 11:06:12 +0800

    first commit

---
+++ script/display.js
@@ -0,0 +1,12 @@
+
+<script >
+		function showItem(id){
+	var obj = document.getElementById("id");
+	obj.style.display="block";
+}
+
+function hiddenItem(id){
+	var obj = document.getElementById("id");
+	obj.style.display="";
+}
+	</script>
